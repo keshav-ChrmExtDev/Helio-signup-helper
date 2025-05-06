684b84464ed24feb4a42963d62ff87d7ed846124
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-05-06 22:54:30 +0530

    feat: add options section to hold all options related to the checker

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import SubmitButton from "@/components/common/submit_button.svelte";
   import type { Submit_button_states } from "@/types";
-
   import NumberFlow from "@number-flow/svelte";
   let response: string | undefined = $state();
   let time_diffrence: number | undefined = $state();
@@ -57,6 +56,30 @@
     <div class="flex gap-2 items-center mx-auto">
       <NumberFlow value={time_diffrence || 0} class="text-3xl " />ms
     </div>
+  </section>
+
+  <!-- Set Auto Clicker on exact time Section -->
+  <section class=" mt-4 flex flex-col gap-2">
+    <div class="flex items-center gap-3">
+      <div class="i-tabler:settings-filled size-6"></div>
+      <p class="text-xl">Options</p>
+    </div>
+    <ul class="list">
+      <li class="list-row flex justify-between">
+        <p class="text-lg">Autoclicker enabled?</p>
+        <label class="toggle text-base-content toggle-xl">
+          <input type="checkbox" />
+          <div
+            class="i-tabler:x size-6 bg-black z-6"
+            aria-label="enabled"
+          ></div>
+          <div
+            class="i-tabler:check size-6 bg-black z-6"
+            aria-label="enabled"
+          ></div>
+        </label>
+      </li>
+    </ul>
   </section>
   {#if response}
     <div role="alert" class="alert alert-info alert-soft">
