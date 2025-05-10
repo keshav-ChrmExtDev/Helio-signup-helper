220c82b7e7dbfc59b4dfb7984d070397c1583bb3
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-05-10 14:32:44 +0530

    feat: add alarms table

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,12 +4,18 @@ interface user_prefrences {
   id: number;
   auto_click_on_set_time_enabled: boolean;
 }
+interface alarm {
+  id: number;
+  time: string;
+}
 const db = new Dexie("helio_host_db") as Dexie & {
   user_prefrences: EntityTable<user_prefrences, "id">;
+  alarms: EntityTable<alarm, "id">;
 };
 
 db.version(1).stores({
   user_prefrences: "++id, auto_click_on_set_time_enabled ",
+  alarms: "++id, time",
 });
 
 db.user_prefrences.toArray().then((data) => {
@@ -19,4 +25,5 @@ db.user_prefrences.toArray().then((data) => {
 });
 
 export type { user_prefrences };
+export type { user_prefrences, alarm };
 export { db };
