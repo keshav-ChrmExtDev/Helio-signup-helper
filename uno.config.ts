6a38d20b883eb3b40c055db81789d906f2ca4f49
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-05-10 10:52:23 +0530

    feat: add daisyui colors, override table class from daisyui

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from "unocss";
-import presetMini from "@unocss/preset-mini";
+import presetMini, { Theme } from "@unocss/preset-mini";
 import presetIcons from "@unocss/preset-icons";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 import { presetDaisy } from "@ameinhardt/unocss-preset-daisy";
+import theme from "daisyui/functions/variables.js";
+const { rules, ...preset } = presetMini();
 export default defineConfig({
   transformers: [transformerVariantGroup()],
-  presets: [presetIcons({}), presetMini(), presetDaisy()],
+  presets: [
+    presetIcons({}),
+    presetMini(),
+    presetDaisy(),
+    {
+      ...preset,
+      rules: rules!.filter(([selector]) => selector !== "table"),
+    },
+  ],
+  theme: {
+    ...(theme as Theme),
+    // colors: colors as Record<string, string>
+  },
 });
